dfd67464c7173aaa1ec4d65f5c6e67417aa82cdc
humhei <humhei@126.com>
2017-11-27 12:45:03 +0800

    Travis Test

--- Suave.OData.LiteDB.Tests/Tests.OData.fs
+++ Suave.OData.LiteDB.Tests/Tests.OData.fs
@@ -25,51 +25,38 @@ let odataRouter() =
     db.Insert({Id=0;Name="test"})|>ignore
     db.Database.GetCollection<Company>().Insert({Id=0;Name="Hello"})|>ignore
     resource "odata/company" (db.Database.GetCollection<Company>()) |> OData.CRUD
-let runWithConfig = runWith defaultConfig
-  
+//use different port to run test in parallel
+let runWithConfig port= 
+  runWith 
+    {defaultConfig with 
+      bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" port ]}
 let ODataTests =
   testList "ODataTests" [
     testCase "OData GetById Test" <| fun _ -> 
-      let ctx=runWithConfig <|odataRouter()
+      let ctx=runWithConfig 9001 <|odataRouter()
       let res=
        ctx
        |>req GET "odata/company(1)" None
        |>ofJson<Company>
-      if not ctx.cts.IsCancellationRequested then 
-         printfn "Dispose Context"
-         disposeContext ctx
-      else  printfn "NoNeed"
-      Thread.Sleep(2000)
       Expect.equal res.Name  "test" "OData GetById Test Corrently" 
     testCase "OData Add Entity Test" <| fun _ -> 
-      let ctx=runWithConfig <|odataRouter()
+      let ctx=runWithConfig 9002 <|odataRouter()
       let newCompany={Id=0;Name="newCompany"}|>toJson
       let data=new StringContent(newCompany)
       let res=
         ctx
         |>req POST "odata/company" (Some data)
         |>ofJson<Company>
-      if not ctx.cts.IsCancellationRequested then 
-         printfn "Dispose Context"
-         disposeContext ctx
-      else  printfn "NoNeed"
-      Thread.Sleep(2000)
       Expect.equal res.Name  "newCompany" "OData Add Entity Test Corrently"  
     testCase "OData Delete Entity Test" <| fun _ -> 
-      let ctx=runWithConfig <|odataRouter()
+      let ctx=runWithConfig 9003 <|odataRouter()
       let res=
         ctx
         |>req DELETE "odata/company(2)" None
         |>ofJson<Company>
-      if not ctx.cts.IsCancellationRequested then 
-         printfn "Dispose Context"
-         disposeContext ctx      
-      else  printfn "NoNeed"
-      Thread.Sleep(2000)
-
       Expect.equal res.Name  "Hello" "OData Delete Entity Test Corrently"  
     testCase "OData Update Entity Test" <| fun _ -> 
-      let ctx=runWithConfig <|odataRouter()
+      let ctx=runWithConfig 9004 <|odataRouter()
 
       let updatedCompany={Id=2;Name="updatedCompany"}|>toJson
       let data=new StringContent(updatedCompany)
@@ -77,14 +64,9 @@ let ODataTests =
         ctx
           |>req PUT "odata/company(2)" (Some data)
           |>ofJson<Company>
-      if not ctx.cts.IsCancellationRequested then 
-         printfn "Dispose Context"
-         disposeContext ctx      
-      else  printfn "NoNeed"
-      Thread.Sleep(2000)
       Expect.equal res.Name  "updatedCompany" "OData Update Entity Test Corrently" 
     testCase "OData Filter Entity Test" <| fun _ -> 
-      let ctx=runWithConfig <|odataRouter()
+      let ctx=runWithConfig 9005 <|odataRouter()
       let res=
         ctx
           |>reqQuery GET "odata/company" "$select=Name"
