69539385370edd864cfbaea32ea2bdcdcccac6ad
humhei <humhei@126.com>
2017-11-27 12:13:42 +0800

    Travis Test

--- Suave.OData.LiteDB.Tests/Tests.OData.fs
+++ Suave.OData.LiteDB.Tests/Tests.OData.fs
@@ -12,6 +12,7 @@ open Suave.OData.LiteDB.Json
 open System.Net.Http
 open Suave.Http
 open System.Net
+open System.Threading
 
 let useDatabase (f: LiteRepository -> WebPart) = 
     let mapper = FSharpBsonMapper()
@@ -24,42 +25,53 @@ let odataRouter() =
     db.Insert({Id=0;Name="test"})|>ignore
     db.Database.GetCollection<Company>().Insert({Id=0;Name="Hello"})|>ignore
     resource "odata/company" (db.Database.GetCollection<Company>()) |> OData.CRUD
-let runWithConfig = runWith {defaultConfig with bindings=[HttpBinding.create HTTP IPAddress.Loopback 8081us ]}
+let runWithConfig = runWith defaultConfig
   
 let ODataTests =
   testList "ODataTests" [
     testCase "OData GetById Test" <| fun _ -> 
+      let ctx=runWithConfig <|odataRouter()
       let res=
-        runWithConfig <|odataRouter()
-        |>req GET "odata/company(1)" None
-        |>ofJson<Company>
-      Expect.equal res.Name  "test" "OData GetById Test Corrently"
+       ctx
+       |>req GET "odata/company(1)" None
+       |>ofJson<Company>
+      if not ctx.cts.IsCancellationRequested then disposeContext ctx
+      Expect.equal res.Name  "test" "OData GetById Test Corrently" 
     testCase "OData Add Entity Test" <| fun _ -> 
+      let ctx=runWithConfig <|odataRouter()
       let newCompany={Id=0;Name="newCompany"}|>toJson
       let data=new StringContent(newCompany)
       let res=
-        runWithConfig <|odataRouter()
+        ctx
         |>req POST "odata/company" (Some data)
         |>ofJson<Company>
-      Expect.equal res.Name  "newCompany" "OData Add Entity Test Corrently"      
+      if not ctx.cts.IsCancellationRequested then disposeContext ctx
+      Expect.equal res.Name  "newCompany" "OData Add Entity Test Corrently"  
     testCase "OData Delete Entity Test" <| fun _ -> 
+      let ctx=runWithConfig <|odataRouter()
       let res=
-        runWithConfig<| odataRouter()
+        ctx
         |>req DELETE "odata/company(2)" None
         |>ofJson<Company>
+      if not ctx.cts.IsCancellationRequested then disposeContext ctx
       Expect.equal res.Name  "Hello" "OData Delete Entity Test Corrently"  
     testCase "OData Update Entity Test" <| fun _ -> 
+      let ctx=runWithConfig <|odataRouter()
+
       let updatedCompany={Id=2;Name="updatedCompany"}|>toJson
       let data=new StringContent(updatedCompany)
       let res=
-        runWithConfig <|odataRouter()
+        ctx
           |>req PUT "odata/company(2)" (Some data)
           |>ofJson<Company>
-      Expect.equal res.Name  "updatedCompany" "OData Update Entity Test Corrently"    
+      if not ctx.cts.IsCancellationRequested then disposeContext ctx
+      Expect.equal res.Name  "updatedCompany" "OData Update Entity Test Corrently" 
     testCase "OData Filter Entity Test" <| fun _ -> 
+      let ctx=runWithConfig <|odataRouter()
       let res=
-        runWithConfig<| odataRouter()
+        ctx
           |>reqQuery GET "odata/company" "$select=Name"
           |>ofJson<list<string>>
-      Expect.equal res ["test";"Hello"] "OData Filter EntityTest Corrently"                    
+      if not ctx.cts.IsCancellationRequested then disposeContext ctx
+      Expect.equal res ["test";"Hello"] "OData Filter EntityTest Corrently"             
   ]
